0eeb9f7cd7a4237a5e29559d686c2609a850a518
Diego <diegortizb@gmail.com>
2023-09-05 14:35:43 -0300

    Primer commit

---
+++ src/test/java/cl/awakelab/miprimersprimg/MiPrimerSprimgApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.awakelab.miprimersprimg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiPrimerSprimgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
